e3b020dc26524da0f0121b086390bcdeefe1769d
Aseem Shrey <aseem.shrey@grofers.com>
2020-01-19 02:20:21 +0530

    3.Added websocket support with flags ( Pg 16-27 )

--- chat/main.go
+++ chat/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"html/template"
 	"log"
 	"net/http"
@@ -19,14 +20,22 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.fileName)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
 	// root
+	var addr = flag.String("addr", ":8080", "The addr of the application.")
+	flag.Parse() // parse the flags
 	http.Handle("/", &templateHandler{fileName: "chat.html"})
+
+	r := newRoom()
+	http.Handle("/room", r)
+	// get the room going
+	go r.run()
+
 	// start the webserver
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe", err)
 	}
 }
