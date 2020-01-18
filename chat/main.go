1eed24777249b49208935b7a468ef94ec7a762b3
Aseem Shrey <aseem.shrey@grofers.com>
2020-01-19 01:27:13 +0530

    2.Loading html from template

--- chat/main.go
+++ chat/main.go
@@ -1,23 +1,30 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-			<html>
-				<head>
-					<title>Chat</title>
-				</head>
-				<body>
-					Let's Chat !
-				</body>
-			</html>
-		`))
+// templ represents a single template
+type templateHandler struct {
+	fileName string
+	once     sync.Once          // to compile the template only once
+	templ    *template.Template // compiled template reference
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.fileName)))
 	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	// root
+	http.Handle("/", &templateHandler{fileName: "chat.html"})
 	// start the webserver
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe", err)
